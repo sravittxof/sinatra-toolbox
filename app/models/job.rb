166c250f8652c491641e050d6539ad6a0a759f4b
sravittxof <travis.fox90@gmail.come>
2020-12-26 20:18:01 -0500

    Set 'distinct' in Tool and Job classes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job  < ActiveRecord::Base
     has_many :job_tools, class_name: 'JobTool'
-    has_many :tools, through: :job_tools
+    has_many :tools, -> { distinct}, through: :job_tools
     belongs_to :user
     validates_associated :job_tools
     #This line calls '.valid?' on associated objects. It should not be used on both ends of the association as that would cause an infinite loop.
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User  < ActiveRecord::Base
     #In vew template we wouuld use the two lines below, for example
     # <%= text_field :person, :email %>
     # <%= text_field :person, :email_confirmation %>
+    # This check is performed only if email_confirmation is not nil. To require confirmation, make sure to add a presence check for the confirmation attribute (we'll take a look at presence later on this guide):
+    # class Person < ActiveRecord::Base
+    #     validates :email, confirmation: true
+    #     validates :email_confirmation, presence: true
+    #   end
+
 end
